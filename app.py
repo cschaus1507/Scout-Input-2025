2d2f2be7c8ba6124e180ed93ee973057f27cd74c
cschaus1507 <christopher.schaus@warlocks1507.com>
2025-04-13 22:04:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -85,5 +85,9 @@ def scout():
     return render_template('scout.html', event_name=event_name, current_match=current_match, assigned_team=assigned_team)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render gives us the PORT environment variable
+    app.run(host='0.0.0.0', port=port)
+
